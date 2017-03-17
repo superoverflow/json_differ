aa8d65b3baca3b935c5d285a492bab9fddf2d151
Superoverflow <cyrus.tang@superoverflow.com>
2017-03-17 20:32:20 +0000

    start writing diff algo

--- differ.py
+++ differ.py
@@ -1,13 +1,34 @@
 #!/usr/bin/python3
 import logging
 import argparse
+import json
 import os
 
+def read_json_from_file(file):
+    
+    with open(file) as f:
+        lines = f.readlines()
+        js_out = json.loads("".join(lines))
+    
+    return js_out
+
+def compare_json(json1, json2):
+    keys1 = json1.keys()
+    keys2 = json2.keys()
+    
+    logging.debug("looking for common keys between keys1 and keys2")
+    logging.debug("looking for extra keys: keys1")
+    logging.debug("looking for extra keys: keys2")    
+    return True
+    
+def compare_two_list(list1, list2):
+    pass
+    
 
 if __name__ == '__main__':
     LOGFMT = ("%(asctime)-15s [%(levelname)-5s] %(filename)-10s:%(lineno)-3d "
               "%(message)s")
-    logging.basicConfig(level=logging.INFO, format=LOGFMT)
+    logging.basicConfig(level=logging.DEBUG, format=LOGFMT)
     
     parser = argparse.ArgumentParser()
     parser.add_argument("-l", "--left", required=False,
@@ -21,3 +42,7 @@ if __name__ == '__main__':
     
     logging.info("start comparing files [%(left)s] vs [%(right)s]"  
         % {'left': left, 'right' : right} )
+        
+    json_left = read_json_from_file(left) 
+    json_right = read_json_from_file(right)
+    compare_json(json_left, json_right)
